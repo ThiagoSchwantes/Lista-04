89b8492a7b7983f3535b164cda2c5d8766237b6d
fran8ctba <geraldo.fvinicius@gmail.com>
2024-04-20 14:43:55 -0300

    criação de classes e getters e setters

---
+++ Cenario04/src/br/edu/up/modelos/Carro.java
@@ -0,0 +1,29 @@
+package br.edu.up.modelos;
+
+public class Carro extends Estacionamento{
+    
+    private String modelo;
+    private String placa;
+    private String cor;
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    
+}
---
+++ Cenario04/src/br/edu/up/modelos/Estacionamento.java
@@ -0,0 +1,15 @@
+package br.edu.up.modelos;
+
+public class Estacionamento {
+    private Integer quantidadeVagas = 10;
+
+    public Integer getQuantidadeVagas() {
+        return quantidadeVagas;
+    }
+
+    public void setQuantidadeVagas(Integer quantidadeVagas) {
+        this.quantidadeVagas = quantidadeVagas;
+    }
+
+    
+}
