6afb37e2927b50a455549e50145551c4d0583e91
Auto Logon <thiagodemoura2015@gmail.com>
2024-04-25 10:42:37 -0300

    Cenario 4 continuado

--- Cenario04/src/br/edu/up/controller/EstacionamentoController.java
+++ Cenario04/src/br/edu/up/controller/EstacionamentoController.java
@@ -1,9 +1,17 @@
 package br.edu.up.controller;
 
-import br.edu.up.modelos.Carro;
+import java.time.LocalTime;
+
+import br.edu.up.models.Carro;
 
 public class EstacionamentoController {
     private Carro carrosEstacionados[] = new Carro[10];
+    private Integer quantCarros = 0;
+
+    private LocalTime periodoManha = LocalTime.of(6, 0);
+    private LocalTime periodoTarde = LocalTime.of(12, 0);
+    private LocalTime periodoNoite = LocalTime.of(18, 0);
+    private LocalTime fechado = LocalTime.of(24, 0);
 
     public Boolean ocuparVaga(Carro carro){
         Boolean estacionou = false;
@@ -12,7 +20,10 @@ public class EstacionamentoController {
             if(eachCarro == null){
                 eachCarro = carro;
                 eachCarro.setHorarioEntrada();
+
+
                 estacionou = true;
+                quantCarros++;
                 break;
             }
         }
@@ -22,6 +33,7 @@ public class EstacionamentoController {
 
     public Boolean desocuparVaga(String placa){
         Boolean desocupou = false;
+        
         for (Carro eachCarro : carrosEstacionados) {
             if(eachCarro.getPlaca().equals(placa)){
                 eachCarro.setHorarioSaida();
@@ -30,5 +42,13 @@ public class EstacionamentoController {
         }
 
         return desocupou;
-    }    
+    }
+
+    public int getQuantPeriodos(Carro carro){
+        if () {
+            
+        }
+    }
+
+
 }
---
+++ Cenario04/src/br/edu/up/views/Menu.java
@@ -0,0 +1,20 @@
+package br.edu.up.views;
+
+import br.edu.up.Prompt;
+
+public class Menu {
+    public void Menu(){
+        
+
+
+    }
+
+
+    public void opcoes(){
+        Prompt.separador();
+        Prompt.imprimir("Digite uma das opções:");
+        Prompt.imprimir("\t1 - Entrada de um carro:");
+        Prompt.imprimir("\t2 - Saída de um carro:");
+        
+    }
+}
