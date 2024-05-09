bd130c2202a9799c2be6d1c81430dc97574e9451
fran8ctba <geraldo.fvinicius@gmail.com>
2024-05-09 17:58:36 -0300

    criando menuPrincipal

---
+++ Cenario10/src/br/edu/up/views/Menu.java
@@ -0,0 +1,56 @@
+package br.edu.up.views;
+import br.edu.up.utils.*;
+
+public class Menu {
+    
+    public void menuPrincipal(){
+
+        Prompt.separador();
+        Prompt.imprimir("MENU PRINCIPAL");
+        Prompt.separador();
+
+        Prompt.imprimir("Digite uma das opções:");
+        Prompt.imprimir("\t1 - Incluir seguro");
+        Prompt.imprimir("\t1 - Localizar seguro");
+        Prompt.imprimir("\t3 - Excluir seguro");
+        Prompt.imprimir("\t4 - Excluir todos os seguros");
+        Prompt.imprimir("\t5 - Listar todos os seguros");
+        Prompt.imprimir("\t6 - Ver quantidade de seguros");
+        Prompt.imprimir("\t6 - Sair");
+
+        int opcao1 = Prompt.lerInteiro("Digite aqui: ");
+
+        switch (opcao1) {
+            case 1:
+                //incluirSeguro();
+                break;
+            case 2:
+                //localizarSeguro();
+                break;
+            case 3:
+                //excluirSeguro();
+                break;
+            case 4:
+                //excluirTodosOsSeguros();
+                break;
+            case 5:
+                //listarTodosSeguros();
+                break;
+            case 6:
+                //quantidadeSeguros();
+                break;
+            case 7:
+                encerrarPrograma();
+                break;
+            default:
+                Prompt.imprimir("Valor Inválido.");
+                menuPrincipal();
+                break;
+        }
+    }
+
+    public void encerrarPrograma(){
+        Prompt.imprimir("Encerrando o programa...");
+        System.exit(7);
+    }
+}
